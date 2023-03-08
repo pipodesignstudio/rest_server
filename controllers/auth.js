3fddb5cb4eff18862b7487f6542136e312944a8b
Pipo Studio <67048178+pipodesignstudio@users.noreply.github.com>
2023-03-08 19:08:12 +0100

    ok

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,39 +1,37 @@
 const { response } = require("express");
 const Usuario = require('../models/user');
 const bcryptjs = require('bcryptjs');
-const { generateJWT } = require("../helpers/create_jwt");
 
 const loginController = async(req, res = response) => {
 
     const { email, password } = req.body;
     try {
-        // Verificar si el email existe
         const user = await Usuario.findOne({ email });
-        if (!user) {
-            return res.status(400).json({
-                msg: "Email no existe"
-            })
-        }
 
+        // Verificar si el email existe
+        if (!user) {
+            return res.status.status(400).json({
+                msg: "Email no existe"
+            });
+        }
         // verificar si el usuario está activo en BBDD
         if (!user.status) {
-            return res.status(400).json({
+            return res.status.status(400).json({
                 msg: "El usuario fue borrado"
-            })
+            });
         }
         // Validar password
         const validPass = bcryptjs.compareSync(password, user.password);
         if (!validPass) {
-            return res.status(400).json({
-                msg: "La contraseña es incorrecta"
-            })
+            return res.status.status(400).json({
+                msg: "Contraseña incorrecta"
+            });
         }
-
         // Generar JWT
-        const token  = await generateJWT(user.id);
+
 
         res.json({
-            user, token
+            msg: 'Login correcto'
         });
     } catch (error) {
         console.log(error);
