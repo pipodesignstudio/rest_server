ab29f81ec5368e7df0ac07790980f7d348776b68
Pipo Studio <67048178+pipodesignstudio@users.noreply.github.com>
2023-03-09 20:19:24 +0100

    ok

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,8 @@
 const { response } = require("express");
 const Usuario = require('../models/user');
 const bcryptjs = require('bcryptjs');
+const { googleVerify } = require("../helpers/google.verify");
+const { generateJWT } = require("../helpers/create_jwt");
 
 const loginController = async(req, res = response) => {
 
@@ -28,10 +30,11 @@ const loginController = async(req, res = response) => {
             });
         }
         // Generar JWT
-
+        const token = await generateJWT(user.id);
 
         res.json({
-            msg: 'Login correcto'
+            msg: 'Login correcto',
+            token
         });
     } catch (error) {
         console.log(error);
@@ -45,12 +48,47 @@ const loginController = async(req, res = response) => {
 
 const googleSignIn = async(req, res = response) => {
 
-    const {id_token} = req.body;
+    const { id_token } = req.body;
+    console.log(id_token);
 
-    return res.status(200).json({
-        msg: 'Tenemos id de google',
-        id_token
-    });
+    try {
+        const { email, name, img } = await googleVerify(id_token);
+
+        // Validar email
+
+        let usuario = await Usuario.findOne({ email });
+
+        if (!usuario) {
+            const data = {
+                name,
+                email,
+                password: 'Sin pass',
+                img,
+                google: true
+            };
+            usuario = new Usuario(data);
+            await usuario.save();
+        }
+
+        if (!usuario.status) {
+            return res.status(401).json({
+                msg: 'Este usuario ha sido borrado'
+            })
+        }
+
+        const token = await generateJWT(user.id);
+        console.log(token);
+
+        res.status(200).json({
+            msg: 'Tenemos id de google',
+            token
+        });
+    } catch (error) {
+        console.log(error);
+        json.status(400).json({
+            msg: 'No se ha podido validar el token'
+        })
+    }
 }
 
 module.exports = {
---
+++ helpers/google.verify.js
@@ -0,0 +1,19 @@
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client(process.env.GOOGLE_ID);
+const googleVerify = async(idToken = '') => {
+    const ticket = await client.verifyIdToken({
+        idToken,
+        audience: process.env.GOOGLE_ID,
+    });
+    const { name: name, picture: img, email: email } = ticket.getPayload();
+    return {
+        name,
+        img,
+        email
+    }
+}
+
+
+module.exports = {
+    googleVerify
+}
